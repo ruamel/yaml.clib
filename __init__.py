bb952ea5814188172facee859cd4c0325afd320a
Anthon van der Neut <anthon@mnt.org>
2019-08-07 22:01:46 +0200

    Added tag 0.1.1 for changeset 15f91a23adef

--- __init__.py
+++ __init__.py
@@ -7,8 +7,8 @@ if False:  # MYPY
 
 _package_data = dict(
     full_package_name='ruamel.yaml.clib',
-    version_info=(0, 1, 1),
-    __version__='0.1.1',
+    version_info=(0, 1, 2),
+    __version__='0.1.2',
     author='Anthon van der Neut',
     author_email='a.van.der.neut@ruamel.eu',
     description='C version of reader, parser and emitter for ruamel.yaml derived from libyaml',
